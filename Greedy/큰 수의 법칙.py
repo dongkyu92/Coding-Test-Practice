8c9565d3ddca54b7443156b247c9c1ba67afbb44
dongkyu92 <dongkyu92@naver.com>
2020-12-20 17:59:32 +0900

    Greedy - 큰 수의 법칙(수학적 풀이)

--- Greedy/큰 수의 법칙.py
+++ Greedy/큰 수의 법칙.py
@@ -2,21 +2,28 @@ N, M, K = map(int, input().split())
 number = list(map(int, input().split()))
 
 number.sort()
-
-first = number[-1:]
-second = number[-2:-1]
+first = number[N - 1]
+second = number[N - 2]
 
 answer = 0
 num = 0
 
 for _ in range(M):
     if num == K:
-        answer += second[0]
+        answer += second
         num = 0
     else:
-        answer += first[0]
+        answer += first
         num += 1
 
 print(answer)
         
-    
+
+# 가장 큰 수가 더해지는 횟수 계산
+
+# count = int(M / (K + 1)) * K
+# count += M % (K + 1)
+
+# result = 0
+# result += (count) * first
+# result += (M - count) * second
