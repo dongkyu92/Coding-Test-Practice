82dbbec2ecf55237b8bc31877c05af0e1437f29f
dongkyu92 <dongkyu92@naver.com>
2021-01-05 09:30:25 +0900

    Greedy - 숫자 카드 게임

--- Greedy/숫자 카드 게임.py
+++ Greedy/숫자 카드 게임.py
@@ -7,7 +7,19 @@ for i in range(n):
     data = list(map(int, input().split()))
     # 현재 줄에서 '가장 작은 수' 찾기
     min_value = min(data)
-    # '가장 작은 수'들 중에서 가장 큰 수 찾기
-    result = max(Result, min_value
+    
+    # 각 행마다 추출한 '가장 작은 수'들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
 
 print(result) # 답 출력
+
+# # 입력 예시 1
+# 3 3
+# 3 1 2
+# 4 1 4
+# 2 2 2
+
+# # 입력 예시 2
+# 2 4
+# 7 3 1 8
+# 3 3 3 4
